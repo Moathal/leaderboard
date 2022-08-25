dee05d05359962ae5f5b497cc10da1c53ece9610
Moathal <moathal.s.kachi@gmail.com>
2022-08-25 21:35:18 +0300

    Add new game to API base
    Add IDs for the elements
    
    Add get promis and post promise
    
    Add event listener for posting
    
    Add event listener for getting values

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './style.css';
+
+const gameAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/PvvOdCYITi0IQGmsheYe/scores/';
+const refresh = document.getElementById('refresh');
+const name = document.getElementById('name');
+const score = document.getElementById('score');
+const submit = document.getElementById('submit');
+const list = document.getElementById('list');
+
+const getValue = async () => {
+    const response = await fetch(gameAPI).then(res => res.json());
+    return response.result;
+};
+
+const postValue = async () => {
+     await fetch(gameAPI, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user: name.value,
+            score: score.value
+        })
+    });
+};
+
+submit.addEventListener('click', () =>{
+    postValue()
+});
+
+refresh.addEventListener('click', () => {
+    console.log(getValue())
+    list.innerText = '';
+    results.forEach((player) => {
+        let li = document.createElement('li');
+        li.innerText = `${player.user}: ${player.score}`;
+        list.appendChild(li);
+    });
+})
